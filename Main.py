de2ad7bfcd0a2ea5e09b1c58cfb96b05dd9a1422
Leopold67 <jayminjhaveri10@gmail.com>
2023-12-05 18:33:41 -0500

    Finished remove command

--- Main.py
+++ Main.py
@@ -25,14 +25,16 @@ cursor.execute("CREATE TABLE IF NOT EXISTS events(server, year, month, day, even
 
 options = (
     "Usage:\n"
-+   "::help         |   Brings up this menu any time\n"
-+   "::add          |   To add an event\n"
-+   "::remove   |   To remove an event\n"
-+   "::view         |   To view this month"
++   "::commands        |   Brings up this menu any time\n"
++   "::add                      |   To add an event\n"
++   "::remove               |   To remove an event\n"
++   "::view                     |   To view this month"
 )
 
-incorrect_add = ("Usage: ::add ['event'] ['(mm/dd/yyyy)']\n*No brackets*")
-incorrect_view = ("Usage: ::view *Optional*['(mm/dd/yyyy)']\n*No brackets*")
+incorrect_add = ("Usage: ::add [\"event\"] ['mm/dd/yyyy']\nUse quotes for events with multiple words\n*No brackets*")
+incorrect_view = ("Usage: ::view *Optional*['mm/dd/yyyy']\n*No brackets*")
+no_special = ("Special characters ': *' not allowed!")
+incorrect_remove = ("Usage: ::remove [\"event\"]['mm/dd/yyyy']\nUse '*' to remove all for a given date\n*No brackets*")
 
 @bot.event
 async def on_ready():
@@ -40,6 +42,10 @@ async def on_ready():
     for guild in bot.guilds:
         print(guild.name)
         print(guild.id)
+
+@bot.command()
+async def commands(ctx, *args):
+    await ctx.send(options)
 
 @bot.command()
 async def test(ctx):
@@ -56,10 +62,13 @@ def parse_month(mon) -> list:
 
 def list_itr(results) -> str:
     out = ""
+    i = 1
     for entry in results:
+        out += str(i) + ")\n"
         out += "Date: " + entry[2] + "/" + entry[3] + "/" + entry[1] + "\n"
         out += "Event: " + entry[4] + "\n"
         out += "\n"
+        i += 1
     return out
 
 @bot.command()
@@ -70,12 +79,15 @@ async def view(ctx, *args):
         thisyear = str(datetime.now().year)
         print(thismonth)
         print(thisyear)
-        # AND year='{thisyear}' AND month='{thismonth}
         query = f"SELECT * FROM events WHERE server={server} AND year='{thisyear}' AND month='{thismonth}'"
-        print(query)
         res = cursor.execute(query)
         out = cursor.fetchall()
-        msg = list_itr(out)
+        listing = list_itr(out)
+        if len(listing) == 0:
+            await ctx.send("NO EVENTS TO SHOW FOR THIS MONTH")
+            return True
+        msg = "ALL EVENTS FROM THIS MONTH:\n"
+        msg += listing
         print(msg)
         if len(msg) == 0:
             await ctx.send("No events to show")
@@ -90,7 +102,6 @@ async def view(ctx, *args):
             month = date[0]
             day = date[1]
             year = date[2]
-            # WHERE server=? AND year=? AND month=? AND day=?"" - (server, year, month, day,)
             query = f"SELECT * FROM events WHERE server={server} AND year='{year}' AND month='{month}' AND day='{day}'"
             res = cursor.execute(query)
             out = cursor.fetchall()
@@ -108,6 +119,8 @@ async def add(ctx, *args):
         if check_err(args[1]):
             await ctx.send(incorrect_add)
             return False
+        elif args[1] == ":" or args[1] == "*":
+            await ctx.send(no_special)
         else:
             event = args[0]
             date = args[1]
@@ -117,10 +130,49 @@ async def add(ctx, *args):
             year = seperate[2]
             query_add = f"INSERT OR IGNORE INTO events (server, year, month, day, event) VALUES ({server}, '{year}', '{month}', '{day}', '{event}')"
             res = cursor.execute(query_add)
-            #out = res.fetchone()
             cursor.connection.commit()
             st = f"Added/Attempted to add: \"{event}\"" + " on " + date + "."
             await ctx.send(st)
+
+@bot.command()
+async def remove(ctx, *args):
+    if len(args) == 0:
+        await ctx.send(incorrect_remove)
+    elif len(args) == 1:
+        event = args[0]
+        if event == "!ALL":
+            query_rem = "DELETE FROM events"
+            res = cursor.execute(query_rem)
+            cursor.connection.commit()
+            st = "REMOVED ALL EVENTS"
+            await ctx.send(st)
+            return True
+        else:
+            await ctx.send(incorrect_remove)
+            return True
+    else:
+        event = args[0]
+        date = args[1]
+        seperate = parse_month(date)
+        month = seperate[0]
+        day = seperate[1]
+        year = seperate[2]
+        # If *
+        if event == "*":
+            query_rem = f"DELETE FROM events WHERE year='{year}' AND month='{month}' AND day='{day}'"
+            res = cursor.execute(query_rem)
+            cursor.connection.commit()
+            st = f"Removed/Attempted to remove: ALL EVENTS on {month}/{day}/{year}"
+            await ctx.send(st)
+            return True
+        else:
+            query_rem = f"DELETE FROM events WHERE year='{year}' AND month='{month}' AND day='{day}' AND event='{event}'"
+            res = cursor.execute(query_rem)
+            cursor.connection.commit()
+            st = f"Removed/Attempted to remove: \"{event}\" on {month}/{day}/{year}"
+            await ctx.send(st)
+            return True
+    return False
 
 @bot.listen('on_message')
 async def send(message):
